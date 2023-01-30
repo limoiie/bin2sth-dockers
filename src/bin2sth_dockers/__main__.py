bdd8252faed5c0f90fc545b4be372826979eb5c4
limoiie <limo.iie4@gmail.com>
2023-01-30 20:47:02 +0800

    refactor: Add annotator for pushing image to registry after building

--- src/bin2sth_dockers/__main__.py
+++ src/bin2sth_dockers/__main__.py
@@ -7,22 +7,30 @@ from typing import Tuple, Union
 
 import fire
 
+DOCKER_REGISTRY = os.getenv('DOCKER_REGISTRY')
+
 ENABLE_PROXY = (os.getenv('ENABLE_PROXY') or '').lower() not in \
                ('', '0', 'false', 'off', 'no', 'none')
 
 
-def docker_root() -> pathlib.Path:
+def docker_root(*parts) -> pathlib.Path:
     path = pathlib.Path(__file__)
 
     while path:
         if (path / 'pyproject.toml').exists():
-            return path / 'docker'
+            return path.joinpath('docker', *parts)
         path = path.parent
 
     raise RuntimeError('Cannot find folder dockers.')
 
 
 def target_os(targets: Union[str, Tuple[str, ...]]):
+    """
+    Annotator that will panic if the host does not match the targets.
+
+    :param targets: The legal host conditions.
+    :return: The wrapped function.
+    """
     if not isinstance(targets, tuple):
         targets = (targets,)
 
@@ -43,6 +51,27 @@ def target_os(targets: Union[str, Tuple[str, ...]]):
         return fn
 
     return wrapped
+
+
+def push_to_registry(fn=None):
+    """
+    Annotator that push the tag to registry defined by env var DOCKER_REGISTRY.
+
+    :param fn: The function that will return a tag going to be pushed.
+    :return: The wrapped function.
+    """
+    if fn is None:
+        return push_to_registry
+
+    @functools.wraps(fn)
+    def push_to_registry_after_fn(*args, **kwargs):
+        tag = fn(*args, **kwargs)
+        if DOCKER_REGISTRY:
+            remote_tag = DOCKER_REGISTRY + '/' + tag
+            Docker.Tag().copy(tag, remote_tag).run()
+        return tag
+
+    return push_to_registry_after_fn
 
 
 class Docker:
@@ -88,6 +117,14 @@ class Docker:
             self.args.append(tag)
             return self
 
+    class Tag(Command):
+        def __init__(self):
+            super().__init__('tag')
+
+        def copy(self, tag, new_tag):
+            self.args.extend((tag, new_tag))
+            return self
+
 
 class BuildHelper:
     """
@@ -97,6 +134,7 @@ class BuildHelper:
 
     @staticmethod
     @target_os('linux')
+    @push_to_registry
     def vcpkg(ubuntu_tag: str, vcpkg_tag: str):
         """
         Build vcpkg image.
@@ -108,18 +146,21 @@ class BuildHelper:
             .tag(f'ubuntu:{ubuntu_tag}') \
             .run()
 
+        tag = f'vcpkg/ubuntu-{ubuntu_tag}:{vcpkg_tag}'
         root = docker_root() / 'cmds' / 'vcpkg'
         Docker.Build() \
             .dockerfile(root / 'Dockerfile') \
-            .tag(f'vcpkg/ubuntu-{ubuntu_tag}:{vcpkg_tag}') \
+            .tag(tag) \
             .build_arg('UBUNTU_TAG', ubuntu_tag) \
             .build_arg('VCPKG_TAG', vcpkg_tag) \
             .enable_proxy_if_required() \
             .context(root) \
             .run()
+        return tag
 
     @staticmethod
     @target_os('linux')
+    @push_to_registry
     def bap(opam_tag: str, bap_version: str):
         """
         Build bap image.
@@ -131,17 +172,20 @@ class BuildHelper:
             .tag(f'ocaml/opam:{opam_tag}') \
             .run()
 
+        tag = f'bap/{opam_tag}:{bap_version}'
         Docker.Build() \
-            .dockerfile(docker_root() / 'cmds' / 'bap' / 'Dockerfile') \
-            .tag(f'bap/{opam_tag}:{bap_version}') \
+            .dockerfile(docker_root('cmds', 'bap', 'Dockerfile')) \
+            .tag(tag) \
             .build_arg('OPAM_TAG', opam_tag) \
             .build_arg('BAP_VERSION', bap_version) \
             .enable_proxy_if_required() \
             .context('.') \
             .run()
+        return tag
 
     @staticmethod
     @target_os('windows')
+    @push_to_registry
     def ida(ida_dir: str, ida_version: str, windows_tag: str = '20H2'):
         """
         Build ida image.
@@ -151,17 +195,20 @@ class BuildHelper:
         :param ida_version: Version of ida.
         :param windows_tag: Tag of windows mage.
         """
+        tag = f'ida/win-{windows_tag.lower()}:{ida_version}'
         Docker.Build() \
-            .dockerfile(docker_root() / 'cmds' / 'ida' / 'Dockerfile') \
-            .tag(f'ida/win-{windows_tag.lower()}:{ida_version}') \
+            .dockerfile(docker_root('cmds', 'ida', 'Dockerfile')) \
+            .tag(tag) \
             .build_arg('IDA_DIR', ida_dir) \
             .build_arg('WINDOWS_TAG', windows_tag) \
             .enable_proxy_if_required() \
             .context('.') \
             .run()
+        return tag
 
     @staticmethod
     @target_os('windows')
+    @push_to_registry
     def rust_windows(rust_version: str, vs_version: str,
                      windows_tag: str = '20H2'):
         """
@@ -175,15 +222,17 @@ class BuildHelper:
             .tag(f'mcr.microsoft.com/windows:{windows_tag}') \
             .run()
 
+        tag = f'rust/windows-msvc:vs-{vs_version}-rust-{rust_version}'
         Docker.Build() \
-            .dockerfile(docker_root() / 'rust' / 'Dockerfile.windows.msvc') \
-            .tag(f'rust/windows-msvc:vs-{vs_version}-rust-{rust_version}') \
+            .dockerfile(docker_root('rust', 'Dockerfile.windows.msvc')) \
+            .tag(tag) \
             .build_arg('RUST_VERSION', rust_version) \
             .build_arg('VS_VERSION', vs_version) \
             .build_arg('WINDOWS_TAG', windows_tag) \
             .enable_proxy_if_required() \
             .context('.') \
             .run()
+        return tag
 
     class Cmdproxy:
         """
@@ -193,6 +242,7 @@ class BuildHelper:
 
         @staticmethod
         @target_os('linux')
+        @push_to_registry
         def vcpkg(ubuntu_tag: str, vcpkg_tag: str, cmdproxy_tag: str):
             """
             Build vcpkg image.
@@ -201,20 +251,23 @@ class BuildHelper:
             :param vcpkg_tag: Name of branch, tag, or commit of repo vcpkg.
             :param cmdproxy_tag: Cmdproxy.rs tag.
             """
+            tag = f'cmdproxy/vcpkg/ubuntu-{ubuntu_tag}:' \
+                  f'cmdproxy-{cmdproxy_tag}-vcpkg-{vcpkg_tag}'
             root = docker_root() / 'cmds' / 'vcpkg'
             Docker.Build() \
                 .dockerfile(root / 'Dockerfile.cmdproxy') \
-                .tag(f'cmdproxy/vcpkg/ubuntu-{ubuntu_tag}:'
-                     f'cmdproxy-{cmdproxy_tag}-vcpkg-{vcpkg_tag}') \
+                .tag(tag) \
                 .build_arg('UBUNTU_TAG', ubuntu_tag) \
                 .build_arg('VCPKG_TAG', vcpkg_tag) \
                 .build_arg('CMDPROXY_TAG', cmdproxy_tag) \
                 .enable_proxy_if_required() \
                 .context(root) \
                 .run()
+            return tag
 
         @staticmethod
         @target_os('linux')
+        @push_to_registry
         def bap(opam_tag: str, bap_version: str, cmdproxy_tag: str):
             """
             Build bap image is embedded with cmdproxy server.
@@ -223,20 +276,22 @@ class BuildHelper:
             :param bap_version: Bap version, only in 2.4.0~2.5.0.
             :param cmdproxy_tag: Cmdproxy.rs tag.
             """
+            tag = f'cmdproxy/bap/{opam_tag}:' \
+                  f'cmdproxy-{cmdproxy_tag}-bap-{bap_version}'
             Docker.Build() \
-                .dockerfile(docker_root() / 'cmds' / 'bap'
-                            / 'Dockerfile.cmdproxy') \
-                .tag(f'cmdproxy/bap/{opam_tag}:'
-                     f'cmdproxy-{cmdproxy_tag}-bap-{bap_version}') \
+                .dockerfile(docker_root('cmds', 'bap', 'Dockerfile.cmdproxy')) \
+                .tag(tag) \
                 .build_arg('OPAM_TAG', opam_tag) \
                 .build_arg('BAP_VERSION', bap_version) \
                 .build_arg('CMDPROXY_TAG', cmdproxy_tag) \
                 .enable_proxy_if_required() \
                 .context('.') \
                 .run()
+            return tag
 
         @staticmethod
         @target_os('windows')
+        @push_to_registry
         def ida(ida_version: str, cmdproxy_tag: str, windows_tag: str = '20H2'):
             """
             Build ida image that is embedded with cmdproxy server.
@@ -245,17 +300,18 @@ class BuildHelper:
             :param cmdproxy_tag: Tag of cmdproxy image.
             :param windows_tag: Tag of windows image.
             """
+            tag = f'cmdproxy/ida/win-{windows_tag.lower()}:' \
+                  f'cmdproxy-{cmdproxy_tag}-ida-{ida_version}'
             Docker.Build() \
-                .dockerfile(docker_root() / 'cmds' / 'ida'
-                            / 'Dockerfile.cmdproxy') \
-                .tag(f'cmdproxy/ida/win-{windows_tag.lower()}:'
-                     f'cmdproxy-{cmdproxy_tag}-ida-{ida_version}') \
+                .dockerfile(docker_root('cmds', 'ida', 'Dockerfile.cmdproxy')) \
+                .tag(tag) \
                 .build_arg('WINDOWS_TAG', windows_tag.lower()) \
                 .build_arg('CMDPROXY_TAG', cmdproxy_tag) \
                 .build_arg('IDA_TAG', ida_version) \
                 .enable_proxy_if_required() \
                 .context('.') \
                 .run()
+            return tag
 
 
 if __name__ == '__main__':
